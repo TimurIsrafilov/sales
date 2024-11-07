0e078f4249fd0765982960e2da33747c468af765
Timur Israfilov <timooooon11@yandex.ru>
2024-11-07 15:50:04 +0300

    settings added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
